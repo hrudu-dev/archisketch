6f3f69ca471d2f26f216934c0480b6a838e4ba3a
Hrudu Shibu <hrudushibu.tech@gmail.com>
2025-09-20 08:26:04 +0000

    name icon not in matching color palete

--- src/components/console/sidebar.tsx
+++ src/components/console/sidebar.tsx
@@ -28,8 +28,8 @@ export function ConsoleSidebar() {
   return (
     <Sidebar className="border-r" side="left">
       <SidebarHeader className="border-b h-16 p-4 flex items-center">
-        <Link href="/console" className="flex items-center gap-2 font-semibold">
-          <Image src={ArchiSketchLogo} alt="ArchiSketch Logo" width={24} height={24} />
+        <Link href="/console" className="flex items-center gap-2 font-semibold text-foreground">
+          <Image src={ArchiSketchLogo} alt="ArchiSketch Logo" width={24} height={24} className="text-primary" />
           {state === 'expanded' && <span className="font-semibold">ArchiSketch</span>}
         </Link>
       </SidebarHeader>
